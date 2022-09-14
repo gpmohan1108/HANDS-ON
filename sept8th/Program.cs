0beba9aa22382470b5f59b881e7ba6078f9bc89f
Mohan <gpmohan12345@gmail.com>
2022-09-14 11:34:52 +0530

    upload

--- sept8th/Program.cs
+++ sept8th/Program.cs
@@ -5,7 +5,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sep08
+namespace Sept8th
 {
     public delegate Dept numb(Dept p);
     public delegate void numb1(List<Employee> m);
